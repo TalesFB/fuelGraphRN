3c86773d13b750995b6f16294b72e3c0a0a710ab
tales dev <tales.ferreira@carteiraglobal.com>
2023-03-14 17:53:05 -0300

    first commit

---
+++ src/screens/Home/styles/theme.ts
@@ -0,0 +1,16 @@
+export default {
+
+    colors:{
+        primary:"#00AF1C",
+        secundary:"#3D3D3D",
+        background:"#161616",
+        blue:"#2B97E6",
+        grayText:"#B6B6B6",
+        grayTextPlacehoder:"#7E7E7E",
+        whiteText:"#FFFFFF",
+        highlight:"#FFFFFF"
+    },
+    fonts:{
+
+    }
+}
