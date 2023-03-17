44ed2ef25ef548edc55ba116c8cef962f969cda8
Tales Ferreira <tales.ferreira@carteiraglobal.com>
2023-03-17 18:27:06 -0300

    fix key map

--- src/components/SupplyList/index.tsx
+++ src/components/SupplyList/index.tsx
@@ -2,15 +2,15 @@ import { ScrollView } from "react-native";
 import { VerticalCard } from "../VerticalCard";
 import { Container, LabelText } from "./styles";
 
-export function SupplyList({list}: any) {
+export function SupplyList({ list }: any) {
   return (
     <>
       <Container>
         <LabelText>Listagem de abastecimentos</LabelText>
         <ScrollView indicatorStyle="white">
           {list?.map((item: any) => (
-          <VerticalCard item={item} />
-        ))}
+            <VerticalCard key={item.id} item={item} />
+          ))}
         </ScrollView>
       </Container>
     </>
