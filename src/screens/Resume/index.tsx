bee44fd75ec6e0cb6886686fee26cfcb24d36b52
Tales Ferreira <tales.ferreira@carteiraglobal.com>
2023-03-25 18:40:37 -0300

    create percentage items

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,16 +1,89 @@
 import { useContext } from "react";
 import { Header } from "../../components/Header";
-import { Container } from "./styles";
+import {
+  Container,
+  ResumeText,
+  ChartContainer,
+  Divider,
+  LabelCharContainer,
+  Item,
+  LabelItem,
+  PointerGas,
+  PointerAlcool,
+  Label,
+  Percent,
+} from "./styles";
 import { VictoryPie } from "victory-native";
+import theme from "../../global/styles/theme";
 
 import { AuthvehicleContext } from "../../context/AuthVehicleContext";
 
 export function Resume() {
   const { vehiclesData, plateCar }: any = useContext(AuthvehicleContext);
-  console.log(vehiclesData, plateCar);
+  const vehicleSupplies = vehiclesData.filter((item: any) => item.id === plateCar)[0].supplies;
+
+  const GasData = vehicleSupplies.filter((item: any) => item.type === "Gasolina");
+  const AlcoolData = vehicleSupplies.filter((item: any) => item.type === "Álcool");
+  const totalGas = GasData.reduce(
+    (accumulator: any, currentValue: any) => accumulator + currentValue.totalValue,
+    0
+  );
+  const totalAlcool = AlcoolData.reduce(
+    (accumulator: any, currentValue: any) => accumulator + currentValue.totalValue,
+    0
+  );
+  const dataChart = [
+    { type: "Gasolina", totalValue: totalGas, color: theme.colors.red },
+    { type: "Álcool", totalValue: totalAlcool, color: theme.colors.primary },
+  ];
+
+  console.log(totalGas, totalAlcool);
+  const totalValue = totalGas + totalAlcool;
+  const percentGas = (totalGas / totalValue) * 100;
+  const percentAlcool = (totalAlcool / totalValue) * 100;
+
   return (
     <Container>
       <Header />
+      <ResumeText>Resumo por tipo de combustível</ResumeText>
+      <ChartContainer>
+        <VictoryPie
+          data={dataChart}
+          padAngle={1}
+          x="type"
+          y="totalValue"
+          colorScale={dataChart.map((item) => item.color)}
+          innerRadius={100}
+          style={{
+            labels: {
+              fill: theme.colors.whiteText,
+              fontSize: 10,
+              display: "none",
+            },
+          }}
+          animate={{
+            duration: 2000,
+            easing: "bounce",
+          }}
+        />
+      </ChartContainer>
+      <Divider />
+      <LabelCharContainer>
+        <Item>
+          <LabelItem>
+            <PointerGas></PointerGas>
+            <Label>Gasolina</Label>
+          </LabelItem>
+          <Percent>{percentGas}%</Percent>
+        </Item>
+        <Item>
+          <LabelItem>
+            <PointerAlcool></PointerAlcool>
+            <Label>Álcool</Label>
+          </LabelItem>
+          <Percent>{percentAlcool}%</Percent>
+        </Item>
+      </LabelCharContainer>
     </Container>
   );
 }
--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -7,3 +7,61 @@ export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
 `;
+export const ResumeText = styled.Text`
+  color: ${({ theme }) => theme.colors.whiteText};
+  font-size: 20px;
+  font-family: ${({ theme }) => theme.fonts.medium};
+  margin: 30px 0 15px 25px;
+`;
+export const ChartContainer = styled.View``;
+
+export const Divider = styled.View`
+  border-bottom-color: white;
+  border-bottom-width: 1px;
+  margin: ${RFValue(15)}px ${RFValue(50)}px;
+`;
+
+export const LabelCharContainer = styled.View`
+  display: flex;
+  flex-direction: column;
+  padding: 0 60px;
+`;
+export const Item = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const LabelItem = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const PointerGas = styled.View`
+  height: 10px;
+  width: 10px;
+  background-color: ${({ theme }) => theme.colors.red};
+  border-radius: 2%;
+  margin-right: 10px;
+`;
+export const PointerAlcool = styled.View`
+  height: 10px;
+  width: 10px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  border-radius: 2%;
+  margin-right: 10px;
+`;
+
+export const Label = styled.Text`
+  color: ${({ theme }) => theme.colors.whiteText};
+  font-size: 15px;
+  font-family: ${({ theme }) => theme.fonts.medium};
+`;
+
+export const Percent = styled.Text`
+  color: ${({ theme }) => theme.colors.whiteText};
+  font-size: 10px;
+  font-family: ${({ theme }) => theme.fonts.medium};
+`;
